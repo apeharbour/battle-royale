d2716d3f34afde74fdeb919fde2feceb4891b71e
jayants <jayant7279@gmail.com>
2023-08-16 13:01:31 +0200

    hardhat config

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -2,7 +2,6 @@ require("@nomicfoundation/hardhat-toolbox");
 
 const INFURA_API_KEY = '6db1336ca92a4314a1cb9bd6deb25103';
 
-const SEPOLIA_PRIVATE_KEY = '9c0521b8049b9cc6f6c7f6a93b93fc31c9a3738dcaee3ae5a8977652b832d828';
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -10,7 +9,7 @@ module.exports = {
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY],
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY],
     },
   },
 };
