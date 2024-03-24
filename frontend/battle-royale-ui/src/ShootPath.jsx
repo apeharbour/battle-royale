68f262ec20f48001fa744a270e28528e2ccb71b3
Christoph Niemann <christoph@niemannsland.de>
2024-03-24 15:10:47 +0100

    Simplify shipPath and shotPath

--- frontend/battle-royale-ui/src/ShootPath.jsx
+++ frontend/battle-royale-ui/src/ShootPath.jsx
@@ -4,33 +4,24 @@ import { Path } from "react-hexgrid";
 export default function ShootPath({ start, end, ...props }) {
   // Create a ref to hold the SVG <path> element
   const pathRef = useRef(null);
-  const [pathData, setPathData] = useState("");
-  const [shipMarker, setShipMarker] = useState("");
+  // const [pathData, setPathData] = useState("");
+  // const [shipMarker, setShipMarker] = useState("");
 
-  useEffect(() => {
-    if (pathRef.current) {
-      // Access the SVG <path> element directly
-      const svgPathElement = pathRef.current.querySelector("path");
-      if (svgPathElement) {
-        // Now you can access the 'd' attribute or any other attributes
-        const pathData = svgPathElement.getAttribute("d");
-        setPathData(pathData);
-        if (start && start.address) {
-          setShipMarker(`url(#pat-${start.address})`);
-        }
-        // Perform any actions you need with the path data
-      }
-    }
-  }, [start, end]); // Empty dependency array means this runs once after the initial render
-
-  const shootPathStyle = {
-    animation: 'path-draw 2s linear forwards infinite',
-    strokeWidth: 0.5, 
-    strokeLinecap: "round", 
-    strokeLinejoin: "round" , 
-    // strokeOpacity: 0.5 
-  };
-
+  // useEffect(() => {
+  //   if (pathRef.current) {
+  //     // Access the SVG <path> element directly
+  //     const svgPathElement = pathRef.current.querySelector("path");
+  //     if (svgPathElement) {
+  //       // Now you can access the 'd' attribute or any other attributes
+  //       const pathData = svgPathElement.getAttribute("d");
+  //       setPathData(pathData);
+  //       if (start && start.address) {
+  //         setShipMarker(`url(#pat-${start.address})`);
+  //       }
+  //       // Perform any actions you need with the path data
+  //     }
+  //   }
+  // }, [start, end]); // Empty dependency array means this runs once after the initial render
 
   return (
     <g ref={pathRef}>
@@ -43,14 +34,10 @@ export default function ShootPath({ start, end, ...props }) {
       <Path
         start={start}
         end={end}
-        // markerMid="url(#shootIcon)"
         markerEnd="url(#shootIcon)"
         className="shoot-path"
 
       />
-      {/* <circle r="2" fill="red">
-        <animateMotion dur="10s" repeatCount="indefinite" path={pathData} />
-      </circle> */}
     </g>
   );
 }
