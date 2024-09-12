a7f161a17acc88ab132c31c38d69cd8bd18e1015
Christoph Niemann <christoph@niemannsland.de>
2024-09-12 11:57:23 +0200

    Add ship paths

--- frontend/battle-royale-ui/src/CoV_Art.jsx
+++ frontend/battle-royale-ui/src/CoV_Art.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
-import { defineHex, Grid, } from "honeycomb-grid";
+import { defineHex, Grid } from "honeycomb-grid";
 import { SVG } from "@svgdotjs/svg.js";
+import { Typography } from "@mui/material";
 
 const shortenAddress = (address) => {
   return `${address.slice(2, 6)}\n${address.slice(-4)}`;
@@ -11,48 +12,124 @@ export default function CoV_Art({
   cells,
   ships,
   gameId,
-  width,
-  height,
 }) {
+  // const svgRef = useRef(null);
 
-  const svgRef = useRef(null);
+  // Create a hexagonal grid using honeycomb
+  const CustomHex = defineHex({
+    dimensions: 30, // size of each hexagon
+    origin: "topLeft",
+  });
 
-  useEffect(() => {
+  const shortenAddress = (address) => {
+    return (
+      address.substring(0, 6) +
+      "..." +
+      address.substring(address.length - 4, address.length)
+    );
+  };
 
-    if (!svgRef.current) return; // Make sure svgRef is available
-
-    console.log(svgRef);
-
-    // Create a hexagonal grid using honeycomb
-    const CustomHex = defineHex({
-      dimensions: 30, // size of each hexagon
-      origin: "topLeft",
+  const drawShips = (draw, ships) => {
+    ships.forEach((ship) => {
+      const hex = new CustomHex({ q: ship.q, r: ship.r });
+      const { x, y } = hex;
+      draw
+        .circle(10)
+        .move(x - 5, y - 5)
+        .fill("red");
     });
+  };
 
-    // Reference to the SVG container
-    const draw = SVG().addTo("body").size(800, 600); // adjust size of the drawing area
+  const drawSquigglyShipPath = (draw, ships) => {
+    ships.forEach((ship) => {
+      const hexes = ship.travelLong.map(
+        (cell) => new CustomHex({ q: cell.q, r: cell.r})
+      );
 
-    // Draw each cell
+      const controlPoint = new CustomHex({
+        q: hexes[1].q - 0.25,
+        r: hexes[1].r - 0.25,
+      });
+
+      const pathString = hexes
+        .map((hex, index) => {
+          const { x, y } = hex;
+          const prefix = index === 0 ? "M" : index === 1 ? `Q${controlPoint.x} ${controlPoint.y}` : "T";
+          // const prefix = index === 0 ? `M ${x} ${y}` : "L";
+          return `${prefix} ${x} ${y}`;
+        })
+        .join(" ");
+
+      draw.path(pathString).fill("none").stroke({ color: "#ff0", width: 2 });
+      draw.circle(5).move(controlPoint.x - 2.5, controlPoint.y - 2.5).fill("#ccc");
+    });
+  };
+
+  const drawStraightShipPath = (draw, ships) => {
+    ships.forEach((ship) => {
+      const hexes = ship.travelLong.map(
+        (cell) => new CustomHex({ q: cell.q, r: cell.r})
+      );
+
+      const pathStringStraight = hexes
+        .map((hex, index) => {
+          const { x, y } = hex;
+          const prefix = index === 0 ? "M" : "L";
+          return `${prefix} ${x} ${y}`;
+        })
+        .join(" ");
+
+        draw.path(pathStringStraight).fill("none").stroke({ color: "#0f0", width: 2 });
+    });
+  };
+
+  const drawHexagons = (draw, cells) => {
     cells.forEach((cell) => {
-      const hex = new CustomHex({ q: cell.q, r: cell.r, s: cell.s});
+      const hex = new CustomHex({ q: cell.q, r: cell.r});
+
+      // Draw the hexagon
+      draw
+        .polygon(hex.corners.map(({ x, y }) => `${x},${y}`))
+        .fill("none")
+        .stroke({ color: "lightgray" });
+    })
+  };
+
+  const drawIslands = (draw, cells) => {
+    cells.forEach((cell) => {
+      const hex = new CustomHex({ q: cell.q, r: cell.r, s: cell.s });
 
       // Get the pixel coordinates of the hexagon
-      console.log(hex);
-      const {x, y} = hex;
-      
-      // Draw the hexagon
-      const hexagon = draw.polygon(hex.corners.map(({ x, y }) => `${x},${y}`)).fill("none").stroke({ color: '#ff0' });
-      
+      const { x, y } = hex;
+
       // If the cell is an island, draw a green dot in the center
       if (cell.island) {
-        draw.circle(10).move(x - 5, y  - 5).fill('green');
+        draw
+          .circle(10)
+          .move(x - 5, y - 5)
+          .fill("green");
       }
     });
+  }
+
+
+  useEffect(() => {
+    // if (!svgRef.current) return; // Make sure svgRef is available
+
+    // Reference to the SVG container
+    const draw = SVG().addTo("#svgDrawing").size(800, 600); // adjust size of the drawing area
+
+
+    // drawHexagons(draw, cells);
+    drawIslands(draw, cells);
+    drawSquigglyShipPath(draw, ships);
+    drawStraightShipPath(draw, ships);
+    drawShips(draw, ships);
   }, [cells]);
 
   return (
-    <div ref={svgRef}></div>
-  );  
-
-
+    <>
+    <div></div>
+    </>
+  );
 }
--- frontend/battle-royale-ui/src/CoV_Data.jsx
+++ frontend/battle-royale-ui/src/CoV_Data.jsx
@@ -263,6 +263,7 @@ export default function CoV_Data(props) {
   const { ref, width, height } = useElementSize();
   console.log("Width: ", width);
   console.log("Height: ", height);
+  console.log("Ships", ships);
 
 
 
@@ -271,12 +272,11 @@ export default function CoV_Data(props) {
       <Grid container spacing={2} p={4}>
         <Grid item xs={12}>
           <Typography variant="h4" align="center">Final Art Display for Game #{gameId}</Typography>
-          <Typography variant="body" align="center">width: {width}, height: {height} </Typography>
+          {/* <Typography variant="body" align="center">width: {width}, height: {height} </Typography> */}
         </Grid>
-        <Grid item xs={12} ref={ref}>
-          {cells && <CoV_Art gameId={gameId} cells={cells} center={center} ships={ships} width={width} height={height} /> }
+        <Grid item xs={12} ref={ref} id='svgDrawing'>
+          {cells && <CoV_Art gameId={gameId} cells={cells} center={center} ships={ships} /> }
         </Grid>
-
 
       </Grid>
     </Fragment>
